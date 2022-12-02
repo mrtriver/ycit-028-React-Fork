4acb35ed64168e020c85c59a593e8c0ae9998f60
Bobby Connolly <bobbyconnolly@bobbys-mbp.lan>
2022-12-01 21:04:11 -0500

    add counter

--- lecture-1-create-react-app/src/App.js
+++ lecture-1-create-react-app/src/App.js
@@ -1,3 +1,20 @@
+import { useState } from "react"
+
 export function App() {
-    return <div></div>
+    const [count, setCount] = useState(0)
+
+    function handleClick() {
+        const newCount = count + 1
+        setCount(newCount)
+    }
+
+    return (
+        <div>
+            Hello World
+            <div>
+                <button onClick={handleClick}>Click to increase</button>
+                <div>{count}</div>
+            </div>
+        </div>
+    )
 }
